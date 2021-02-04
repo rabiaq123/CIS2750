d68a7339b1fe333f0eb634849e19f83fccb958de
Rabia Qureshi <rqureshi@uoguelph.ca>
2021-02-04 12:18:07 -0500

    Implemented a simple GPXdoc parser that extracts root node's attributes

--- include/GPXHelpers.h
+++ include/GPXHelpers.h
@@ -1,6 +1,6 @@
 /**
  * Name: Rabia Qureshi
- * Student ID: 1046427
+ * StudentID: 1046427
  * Email: rqureshi@uoguelph.ca 
  */
 
@@ -10,14 +10,43 @@
 #include "LinkedListAPI.h"
 #include <string.h>
 
-/**
- * prints the names of  all the XML elements
- * that are siblings or children of a given xml node
- */
-static void printElementNames(xmlNode*)
 
 /**
- * read XML file & do error-checking and 
- * call function to print element names
+ * traverse XML tree formed with file and parse contents into GPXdoc object
+ * @param node pointer to next node to be traversed in XML tree. Will begin with root node
+ * @param myGPXdoc pointer to GPX struct in which to store entire GPX file
+ * @return boolean value representing whether GPX struct could be created
  */
-int parseXMLfile(char*);
+bool traverseGPXtree(xmlNode* node, GPXdoc* myGPXdoc);
+
+/**
+ * iterate through every attribute of the current node and store in GPX struct
+ * @param curNode current node being updated to store GPX file information
+ * @param myGPXdoc pointer to GPX struct in which to store entire GPX file
+ * @return boolean value representing whether all attributes could be parsed into GPX struct
+ */
+bool storeGPXattributes(xmlNode *curNode, GPXdoc *myGPXdoc);
+
+/**
+ * store the 'namespace' content in GPX struct
+ * @param rootNode level 0 node, 'gpx'
+ * @param myGPXdoc pointer to GPX struct in which to store entire GPX file
+ * @return boolean value representing whether valid 'namespace' was found - halt traversal process if false
+ */
+bool storeGPXnamespace(xmlNode *rootNode, GPXdoc *myGPXdoc);
+
+/**
+ * store the content of the 'version' attribute in GPX struct
+ * @param value value associated with the 'version' attribute
+ * @param myGPXdoc pointer to GPX struct in which to store entire GPX file
+ * @return boolean value representing whether valid 'version' number was found - halt traversal process if false
+ */
+bool storeVersion(xmlNode *value, GPXdoc *myGPXdoc);
+
+/**
+ * store the content of the 'creator' attribute in GPX struct
+ * @param value value associated with the 'version' attribute
+ * @param myGPXdoc pointer to GPX struct in which to store entire GPX file
+ * @return boolean value representing whether valid 'creator' value was found - halt traversal process if false
+ */
+bool storeCreator(xmlNode* value, GPXdoc* myGPXdoc);
--- src/GPXParser.c
+++ src/GPXParser.c
@@ -1,7 +1,7 @@
 /**
  * Name: Rabia Qureshi
- * Student ID: 1046427
- * Email: rqureshi@uoguelph.ca 
+ * StudentID: 1046427
+ * Email: rqureshi@uoguelph.ca
  */
 
 #include <stdio.h>
@@ -16,63 +16,174 @@
 /**
  * Note that as allowed by Professor Nikitenko,
  * certain functions, specifically parseXMLfiles() and printElementNames(),
- * contain sample code from the libXmlExample.c file provided in class, 
- * which is based on the LibXML example from: 
+ * contain sample code from the libXmlExample.c file provided in class,
+ * which is based on the LibXML example from:
  * http://www.xmlsoft.org/examples/tree1.c
  */
 
 
-static void printElementNames(xmlNode* node) {
-    xmlNode *curNode = NULL;
-
-    for (curNode = node; curNode != NULL; curNode = curNode->next) {
-        if (curNode->type == XML_ELEMENT_NODE) { //if the XML element type is node
-            printf("node type: Element, name: %s\n", curNode->name);
-        }
-
-        //uncomment the code below if you want to see the content of every node
-        /*
-        if (cur_node->content != NULL ){
-            printf("  content: %s\n", cur_node->content);
-        }
-        */
-        
-        //iterate through every attribute of the current node
-        xmlAttr *attr;
-        for (attr = curNode->properties; attr != NULL; attr = attr->next) {
-            xmlNode *value = attr->children;
-            char *attrName = (char *)attr->name;
-            char *cont = (char *)(value->content);
-            printf("\tattribute name: %s, attribute value = %s\n", attrName, cont);
-        }
-
-        printElementNames(curNode->children);
-    }
-}
-
-int parseXMLfile(char* filename) {
+GPXdoc* createGPXdoc(char* fileName) {
     /*
      * initializes the library and checks potential ABI mismatches
      * between the version it was compiled for and the actual shared
      * library used.
      */
     LIBXML_TEST_VERSION
+    //printf("in createGPXdoc()");
 
-    xmlDoc *file = NULL;
-    xmlNode *rootElement = NULL;
-
-    //parse the XML file
-    file = xmlReadFile(filename, NULL, 0);
-    if (file == NULL) { //error-checking for invalid XML file
-        return 1;
+    //error-checking for invalid filename
+    if (fileName == NULL || strcmp(fileName, "") == 0) {
+        return NULL;
     }
 
-    //get the root element node and print element names
+    xmlDoc* file = NULL;
+    xmlNode* rootElement = NULL;
+
+    //attempt to parse XML file
+    file = xmlReadFile(fileName, NULL, 0);
+    if (file == NULL) { //return if not a well-formed XML
+        return NULL;
+    }
+    //get the root element node
     rootElement = xmlDocGetRootElement(file);
-    printElementNames(rootElement);
+
+    GPXdoc* myGPXdoc = (GPXdoc*)calloc(1, sizeof(GPXdoc)); //calloc() initializes memory allocated to 0
+    if (!traverseGPXtree(rootElement, myGPXdoc)) { //error-checking for incorrectly-formatted GPX file
+        return NULL;
+    }
 
     xmlFreeDoc(file);
     xmlCleanupParser(); //free the global variables that may have been allocated by the parser
 
-    return 0;
+    return myGPXdoc;
+}
+
+
+bool traverseGPXtree(xmlNode* node, GPXdoc* myGPXdoc) {
+    //printf("in traverseGPXtree()\n");
+    xmlNode* curNode = NULL;
+
+    for (curNode = node; curNode != NULL; curNode = curNode->next) {
+        if (curNode->type == XML_ELEMENT_NODE) {
+
+            //load XML tree into GPX struct
+            if (strcmp((char*)curNode->name, "gpx") == 0) {
+                //error-checking for non-compliance with GPXParser.h documentation
+                if (!storeGPXattributes(curNode, myGPXdoc)) { //store root node's attributes
+                    return false;
+                }
+            }
+        }
+
+        traverseGPXtree(curNode->children, myGPXdoc);
+    }
+
+    return true;
+}
+
+
+bool storeGPXattributes(xmlNode* curNode, GPXdoc* myGPXdoc) {
+    //printf("in storeGPXattributes()");
+    xmlAttr* attr;
+
+    //store the namespace
+    if (!storeGPXnamespace(curNode, myGPXdoc)) { //empty or non-existent ns
+        return false;
+    }
+
+    //NOTE that libxml2 documentation calls xmlAttr values “properties” instead of “attributes”
+    for (attr = curNode->properties; attr != NULL; attr = attr->next) {
+        xmlNode* value = attr->children; //value associated with attribute
+
+        if (strcmp((char*)attr->name, "version") == 0) {
+            if (!storeVersion(value, myGPXdoc)) {
+                return false;
+            }
+        } else if (strcmp((char*)attr->name, "creator") == 0) {
+            if (!storeCreator(value, myGPXdoc)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+
+bool storeCreator(xmlNode* value, GPXdoc* myGPXdoc) {
+    //printf("in storeCreator()");
+    char* buffer;
+    int len;
+
+    //error-checking for incorrectly formatted creator value
+    if(value->content == NULL) {
+        return false;
+    }
+
+    len = strlen((char*)value->content); //stren() excludes NULL terminator
+    buffer = (char*)calloc(len + 1, sizeof(char));
+    strcpy(buffer, (char*)value->content);
+
+    if (buffer[0] == '\0' || strcmp(buffer, "") == 0) { //empty string
+        return false;
+    }
+    //printf("strlen of buffer in storeCreator: %d\n", len);
+
+    //store in GPX struct
+    myGPXdoc->creator = (char *)calloc(len + 1, sizeof(char)); //allocate space for content of type char* in GPX struct
+    strcpy(myGPXdoc->creator, buffer);
+
+    free(buffer);
+
+    return true;
+}
+
+
+bool storeVersion(xmlNode* value, GPXdoc* myGPXdoc) {
+    //printf("in storeVersion()");
+    double versionNum;
+    char* ptr;
+    char* buffer;
+    int len;
+
+    //error-checking for incorrectly formatted version number
+    if (value->content == NULL) {
+        return false;
+    }
+
+    len = strlen((char*)value->content); //stren() excludes NULL terminator
+    buffer = (char*)calloc(len + 1, sizeof(char));
+    strcpy(buffer, (char*)value->content);
+
+    if (strcmp(buffer, "\0") == 0 || strcmp(buffer, "") == 0) { //empty string, GPX version uninitialized
+        return false;
+    }
+
+    //store in GPX struct
+    versionNum = strtod(buffer, &ptr); //convert char* to type double
+    myGPXdoc->version = versionNum;
+
+    free(buffer);
+
+    return true;
+}
+
+
+bool storeGPXnamespace(xmlNode* rootNode, GPXdoc* myGPXdoc) {
+    //printf("in storeGPXnamespace()");
+    char buffer[256] = {'\0'};
+
+    //error-checking for incorrectly formatted namespace
+    if (rootNode->ns == NULL) {
+        return false;
+    }
+    strcpy(buffer, (char*)rootNode->ns->href);
+    if (buffer == '\0' || strcmp(buffer, "") == 0) { //empty string
+        return false;
+    }
+
+    //store in GPX struct
+    strcpy(myGPXdoc->namespace, buffer);
+
+    return true;
 }
